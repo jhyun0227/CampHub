b9fe756180caf13250fd21ebad57f967af6cd4a3
junho <jhyun0227@uniess.co.kr>
2023-08-08 15:38:29 +0900

    초기셋팅

---
+++ src/main/java/com/project/camphub/CamphubApplication.java
@@ -0,0 +1,13 @@
+package com.project.camphub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamphubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamphubApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/camphub/config/ProjectConfig.java
@@ -0,0 +1,15 @@
+package com.project.camphub.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
