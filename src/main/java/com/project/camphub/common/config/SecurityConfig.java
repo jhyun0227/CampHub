c312432d46376d6f4addee63648060c56d9ba9e5
junho <jhyun0227@uniess.co.kr>
2023-09-05 13:39:58 +0900

    feat. OAuth2.0 시작

---
+++ src/main/java/com/project/camphub/common/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.project.camphub.common.config;
+
+import com.project.camphub.login.oauth.OAuth2UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //Security 활성화 의미, Security 필터가 Spring filter체인에 등록
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final OAuth2UserServiceImpl oAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+
+                /**
+                 * 인증 및 인가 설정
+                 */
+                .authorizeRequests()
+                .antMatchers("/", "/login", "/css/**", "/images/**", "/js/**").permitAll()
+//                  .antMatchers("/admin/**").hasRole("ROLE_ADMIN")
+                .anyRequest().authenticated()
+
+                /**
+                 * 소셜 로그인 설정
+                 */
+                .and()
+                .oauth2Login()
+                .loginPage("/login") //Security 기본 로그인 기능 사용하지 않음
+                .userInfoEndpoint() // 클라이언트가 AccessToken을 이용해서 가져온 정보를 사용하는데 사용되는 서비스를 지정한다.
+                .userService(customOAuth2UserService);
+
+        return httpSecurity.build();
+    }
+}
