a6851358b59e90bc0feac89742c95e10cba09395
junho <jhyun0227@uniess.co.kr>
2024-04-04 14:16:40 +0900

    config: 시군구 DML 작성

---
+++ src/main/resources/db/DML.sql
@@ -0,0 +1,287 @@
+### 시도
+INSERT INTO province_code(prov_cd_nm) VALUES ('서울시');
+INSERT INTO province_code(prov_cd_nm) VALUES ('부산시');
+INSERT INTO province_code(prov_cd_nm) VALUES ('대구시');
+INSERT INTO province_code(prov_cd_nm) VALUES ('인천시');
+INSERT INTO province_code(prov_cd_nm) VALUES ('광주시');
+INSERT INTO province_code(prov_cd_nm) VALUES ('대전시');
+INSERT INTO province_code(prov_cd_nm) VALUES ('울산시');
+INSERT INTO province_code(prov_cd_nm) VALUES ('세종시');
+INSERT INTO province_code(prov_cd_nm) VALUES ('경기도');
+INSERT INTO province_code(prov_cd_nm) VALUES ('강원도');
+INSERT INTO province_code(prov_cd_nm) VALUES ('충청북도');
+INSERT INTO province_code(prov_cd_nm) VALUES ('충청남도');
+INSERT INTO province_code(prov_cd_nm) VALUES ('전라북도');
+INSERT INTO province_code(prov_cd_nm) VALUES ('전라남도');
+INSERT INTO province_code(prov_cd_nm) VALUES ('경상북도');
+INSERT INTO province_code(prov_cd_nm) VALUES ('경상남도');
+INSERT INTO province_code(prov_cd_nm) VALUES ('제주도');
+
+### 서울시 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('강남구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('강동구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('강북구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('강서구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('관악구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('광진구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('구로구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('금천구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('노원구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('도봉구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동대문구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동작구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('마포구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서대문구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서초구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('성동구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('성북구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('송파구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('양천구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영등포구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('용산구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('은평구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('종로구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('중구', 1);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('중랑구', 1);
+
+### 부산시 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('강서구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('금정구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('기장군', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동래구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('부산진구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('북구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('사상구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('사하구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('수영구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('연제구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영도구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('중구', 2);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('해운대구', 2);
+
+### 대구시 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남구', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('달서구', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('달성군', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동구', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('북구', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('상주', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서구', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('수성구', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('중구', 3);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('군위군', 3);
+
+### 인천시 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('강화군', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('계양구', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남구', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남동구', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동구', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('부평구', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서구', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('연수구', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('옹진군', 4);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('중구', 4);
+
+### 광주시 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('광산구', 5);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남구', 5);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동구', 5);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('북구', 5);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서구', 5);
+
+### 대전시 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('대덕구', 6);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동구', 6);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서구', 6);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('유성구', 6);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('중구', 6);
+
+### 울산시 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남구', 7);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동구', 7);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('북구', 7);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('울주군', 7);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('중구', 7);
+
+### 세종시 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('금남면', 8);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('세종시', 8);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('소정면', 8);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('연서면', 8);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('전동면', 8);
+
+### 경기도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('가평군', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('고양시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('과천시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('광명시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('광주시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('구리시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('군포시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('김포시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남양주시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동두천시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('부천시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('성남시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('수원시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('시흥시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('안산시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('안성시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('안양시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('양주시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('양평군', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('여주시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('연천구', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('오산시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('용인시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('의왕시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('의정부시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('이천시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('파주시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('평택시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('포천시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('하남시', 9);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('화성시', 9);
+
+### 강원도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('강릉시', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('고성군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('동해시', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('삼척시', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('속초시', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('양구군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('양양군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영월군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('원주시', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('인제군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('정선군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('철원군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('춘천시', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('태백시', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('평창군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('홍천군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('화천군', 10);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('횡성군', 10);
+
+### 충청북도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('괴산군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('단양군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('보은군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영동군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('옥천군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('음성군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('제천시', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('증평군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('진천군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('청원군', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('청주시', 11);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('충주시', 11);
+
+### 충청남도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('계룡시', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('공주시', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('금산군', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('논산시', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('당진시', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('보령시', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('부여군', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서산시', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서천군', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('아산시', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('예산군', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('천안시', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('청양군', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('태안군', 12);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('홍성군', 12);
+
+### 전라북도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('고창군', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('군산시', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('김제시', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남원시', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('무주군', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('부안군', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('순창군', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('완주군', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('익산시', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('임실군', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('장수군', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('전주시', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('정읍시', 13);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('진안군', 13);
+
+### 전라남도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('강진군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('고흥군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('곡성군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('광양시', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('구례군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('나주시', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('담양군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('목포시', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('무안군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('보성군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('순천시', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('신안군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('여수시', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영광군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영암군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('완도군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('장성군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('장흥군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('진도군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('함평군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('해남군', 14);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('화순군', 14);
+
+### 경상북도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('경산시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('경주시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('고령군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('구미시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('김천시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('문경시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('봉화군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('상주시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('성주군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('안동시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영덕군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영양군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영주시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('영천시', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('예천군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('울릉군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('울진군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('의성군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('청도군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('청송군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('칠곡군', 15);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('포항시', 15);
+
+### 경상남도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('거제시', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('거창군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('고성군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('김해시', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('남해군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('밀양시', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('사천시', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('산청군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('양산시', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('의령군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('진주시', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('창녕군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('창원시', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('통영시', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('하동군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('함안군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('함양군', 16);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('합천군', 16);
+
+### 제주도 시군구
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('서귀포시', 17);
+INSERT INTO district_code(dist_cd_nm, prov_cd_id) VALUE ('제주시', 17);
